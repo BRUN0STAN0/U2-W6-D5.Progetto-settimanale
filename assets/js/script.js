ad66807fdc7a54788f6122e82555f0460345e80d
Bruno <brunostano00@protonmail.com>
2022-10-07 16:34:14 +0200

    80%

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+setTimeout(loading,000) //! RICORDATI DI MODIFICARE
+
+function loading() {
+    let spinnerContainer = document.querySelector(".spinner-container");
+    spinnerContainer.style.display = "none"
+}
