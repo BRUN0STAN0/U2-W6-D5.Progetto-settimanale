f0537b5276ebd93d9ff8e25a1e4af26059672fcb
Bruno <brunostano00@protonmail.com>
2022-10-07 18:32:59 +0200

    86%

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,8 @@ function loading() {
     let spinnerContainer = document.querySelector(".spinner-container");
     spinnerContainer.style.display = "none"
 }
+
+window.onscroll = function(e) {
+    let img = document.querySelectorAll('img');
+        img.forEach(element => {element.classList.toggle('fadein')})
+}
