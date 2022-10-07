1049d7d1c540b09fe7e49ef25f87aca76a13ee72
Bruno <brunostano00@protonmail.com>
2022-10-07 20:24:20 +0200

    95%

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,23 @@ window.onscroll = function(e) {
     let img = document.querySelectorAll('img');
         img.forEach(element => {element.classList.toggle('fadein')})
 }
+
+let invertMode = document.getElementById("invert-mode");
+invertMode.addEventListener("click", function () {document.body.classList.toggle("invert-mode")});
+
+
+let lightMode = document.getElementById("light-mode");
+lightMode.addEventListener("click", function () {
+    document.getElementById("home").classList.toggle("light-mode");
+    document.getElementById("partnership").classList.toggle("light-mode");
+    document.querySelector("footer").classList.toggle("light-mode");
+    let paragraph = document.querySelectorAll("p");
+    paragraph.forEach(element => {
+        element.classList.toggle("invert-mode");
+    });
+    document.getElementById("testimonianze").classList.toggle("invert-mode");
+    document.querySelectorAll(".w-50").forEach(e => {
+        e.classList.toggle("invert-mode")
+    })
+})
+
