bb0d5ac70e2dd7ead41760f3e9143b5e0da6f012
Bruno <brunostano00@protonmail.com>
2022-10-09 11:47:25 +0200

    97%

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,8 +28,47 @@ window.onscroll = function(e) {
 
 // * START REVERSE MODE
 let invertMode = document.getElementById("invert-mode");
-invertMode.addEventListener("click", function () {document.body.classList.toggle("invert-mode")});
+invertMode.addEventListener("click", invertHeartMode);
 // ! END REVERSE MODE
+
+// * START HEART MODE
+let heartMode = document.getElementById("heart-mode");
+let clicked = false;
+let interval 
+let rexAudio = new Audio("https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3");
+let heartAudio = new Audio("https://www.soundjay.com/human/sounds/heartbeat-04.mp3");
+heartMode.addEventListener("click", function () {
+    if (clicked == true) {
+        clicked = false;
+        heartMode.children[0].classList.toggle("bi-heart-pulse");
+        heartMode.children[0].classList.toggle("bi-heart-pulse-fill");
+        document.body.classList.remove("invert-mode")
+        rexAudio.pause();
+        heartAudio.pause();
+        clearInterval(heart);
+    } else {
+        clicked = true;
+        heartMode.children[0].classList.toggle("bi-heart-pulse");
+        heartMode.children[0].classList.toggle("bi-heart-pulse-fill");
+        document.body.classList.add("invert-mode")
+        rexAudio.currentTime = 0;
+        heartAudio.currentTime = 0;
+        rexAudio.play();
+        heartAudio.play();
+        heart = setInterval(invertHeartMode, 300)
+    }
+});
+
+function start() {
+    heart = setInterval(invertHeartMode, 300)
+}
+function stop() {
+    clearInterval(invertHeartMode)
+}
+function invertHeartMode() {
+    document.body.classList.toggle("invert-mode")
+}
+// ! END HEART MODE
 
 // * START LIGHTMODE
 let lightMode = document.getElementById("light-mode");
